2715b371208ac245624d570da2ad3a09fc0411ab
LeoTubarão <l.tubarao.santos@gmailcom>
2019-03-04 02:35:17 -0300

    Mudança de pasta os arquivos JS

--- js/modules/validate-form.js
+++
@@ -1,7 +0,0 @@
-export default function initValidateForm() {
-    $('[data-btn]').click(function () {
-        if($("#frm-contato")[0].checkValidity()) { // IF TRUE
-            alert("form submitting");
-        }
-    });
-}
